65e6bd84fe2ae6849e78476647d961d393c83375
FRCTEAM1474 <frcteam1474@gmail.com>
2025-05-02 15:58:05 -0400

    led rainbow pattern, should work. need to test pending led wiring completion.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LEDSubsystem;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -19,6 +20,7 @@ public class Robot extends TimedRobot {
 
   private final RobotContainer m_robotContainer;
 
+  public static LEDSubsystem ledSubsystem;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -27,6 +29,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    ledSubsystem = new LEDSubsystem();
 
     CameraServer.startAutomaticCapture();
 
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
@@ -9,8 +13,16 @@ public class LEDSubsystem extends SubsystemBase{
     private final AddressableLED m_led = new AddressableLED(9);
     private final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
     private final LEDPattern m_test = LEDPattern.rainbow(255, 128);
-    private final Distance kLEDSpacing = Meters.of()
+    private final Distance kLEDSpacing = Meters.of(1/62.5);
+    private final LEDPattern rainbowPattern = m_test.scrollAtAbsoluteSpeed(MetersPerSecond.of(0.25), kLEDSpacing);
     public LEDSubsystem() {
+        m_led.setLength(m_ledBuffer.getLength());
+        m_led.setData(m_ledBuffer);
+        m_led.start();
+    }
+    @Override
+    public void periodic() {
+        rainbowPattern.applyTo(m_ledBuffer);
         m_led.setData(m_ledBuffer);
     }
 }
